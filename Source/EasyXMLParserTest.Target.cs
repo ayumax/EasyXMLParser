f92e0b21ee805059554505ef2256a0c82d579540
ayumax <develop@ayumax.net>
2025-05-16 20:06:35 +0900

    Fixed the scripts to allow build with UE5.5

--- Source/EasyXMLParserTest.Target.cs
+++ Source/EasyXMLParserTest.Target.cs
@@ -1,13 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class EasyXMLParserTestTarget : TargetRules
 {
 	public EasyXMLParserTestTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		CppStandard = CppStandardVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "EasyXMLParserTest" } );
 	}
